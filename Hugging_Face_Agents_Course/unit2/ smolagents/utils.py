95e14086d14031d0ed0ad467b5131cd1c1b9584f
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2025-03-16 09:57:24 +0000

    Add functions for LiteLLM and OpenAIServer model initialization

--- Hugging_Face_Agents_Course/unit2/ smolagents/utils.py
+++ Hugging_Face_Agents_Course/unit2/ smolagents/utils.py
@@ -1,3 +1,17 @@
 from smolagents import LiteLLMModel
+from smolagents import OpenAIServerModel
+import os
 
-gemini_model = LiteLLMModel(model_id="gemini/gemini-2.0-flash")
+
+def gemini_model_liteLLM(model: str) -> LiteLLMModel:
+    """model for example, gemini-2.0-flash"""
+    return LiteLLMModel(model_id=f"gemini/{model}")
+
+
+def gemini_model_OpenAIServer(model: str) -> OpenAIServerModel:
+    model = OpenAIServerModel(
+        model_id=model,
+        api_base="https://generativelanguage.googleapis.com/v1beta/openai/",
+        api_key=os.environ["GOOGLE_API_KEY"],
+    )
+    return model
