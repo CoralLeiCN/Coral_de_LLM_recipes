9220714939658ab2b2c22cc40acebaba1b57d5f2
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2025-05-19 21:42:46 +0100

    Add GuestInfoRetrieverTool for retrieving gala guest information

---
+++ Hugging_Face_Agents_Course/unit3/retriever.py
@@ -0,0 +1,28 @@
+import datasets
+from langchain.docstore.document import Document
+from langchain_community.retrievers import BM25Retriever
+from smolagents import CodeAgent, Tool
+from utils import gemini_model_OpenAIServer
+
+
+class GuestInfoRetrieverTool(Tool):
+    name = "guest_info_retriever"
+    description = "Retrieves detailed information about gala guests based on their name or relation."
+    inputs = {
+        "query": {
+            "type": "string",
+            "description": "The name or relation of the guest you want information about.",
+        }
+    }
+    output_type = "string"
+
+    def __init__(self, docs):
+        self.is_initialized = False
+        self.retriever = BM25Retriever.from_documents(docs)
+
+    def forward(self, query: str):
+        results = self.retriever.get_relevant_documents(query)
+        if results:
+            return "\n\n".join([doc.page_content for doc in results[:3]])
+        else:
+            return "No matching guest information found."
--- Hugging_Face_Agents_Course/unit3/tools.py
+++ Hugging_Face_Agents_Course/unit3/tools.py
@@ -3,8 +3,8 @@ from langchain.docstore.document import Document
 from langchain_community.retrievers import BM25Retriever
 from smolagents import CodeAgent, Tool
 
-from .utils import gemini_model_OpenAIServer
-
+from utils import gemini_model_OpenAIServer
+from retriever import GuestInfoRetrieverTool
 # Load the dataset
 guest_dataset = datasets.load_dataset("agents-course/unit3-invitees", split="train")
 
@@ -24,28 +24,6 @@ docs = [
     for guest in guest_dataset
 ]
 
-
-class GuestInfoRetrieverTool(Tool):
-    name = "guest_info_retriever"
-    description = "Retrieves detailed information about gala guests based on their name or relation."
-    inputs = {
-        "query": {
-            "type": "string",
-            "description": "The name or relation of the guest you want information about.",
-        }
-    }
-    output_type = "string"
-
-    def __init__(self, docs):
-        self.is_initialized = False
-        self.retriever = BM25Retriever.from_documents(docs)
-
-    def forward(self, query: str):
-        results = self.retriever.get_relevant_documents(query)
-        if results:
-            return "\n\n".join([doc.page_content for doc in results[:3]])
-        else:
-            return "No matching guest information found."
 
 
 # Initialize the tool
