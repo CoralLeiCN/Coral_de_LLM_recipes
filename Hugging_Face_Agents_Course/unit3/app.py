2871c68b825d7e13446bb46451ad428e60005298
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2025-05-20 07:19:10 +0100

    The Complete Agent with all tools

--- Hugging_Face_Agents_Course/unit3/app.py
+++ Hugging_Face_Agents_Course/unit3/app.py
@@ -1,12 +1,16 @@
-import random
-
+# Import necessary libraries
 import datasets
 from langchain.docstore.document import Document
-from langchain_community.retrievers import BM25Retriever
 from retriever import GuestInfoRetrieverTool
-from smolagents import CodeAgent, DuckDuckGoSearchTool, Tool
-from tools import WeatherInfoTool, HubStatsTool
+from smolagents import CodeAgent, DuckDuckGoSearchTool
+
+# Import our custom tools from their modules
+from tools import HubStatsTool, WeatherInfoTool
 from utils import gemini_model_OpenAIServer
+
+
+# Initialize the Hugging Face model
+model = gemini_model_OpenAIServer("gemini-2.0-flash")
 
 # Load the dataset
 guest_dataset = datasets.load_dataset("agents-course/unit3-invitees", split="train")
@@ -27,22 +31,8 @@ docs = [
     for guest in guest_dataset
 ]
 
-
 # Initialize the tool
 guest_info_tool = GuestInfoRetrieverTool(docs)
-
-
-# Initialize the Hugging Face model
-model = gemini_model_OpenAIServer("gemini-2.0-flash")
-# Create Alfred, our gala agent, with the guest info tool
-alfred = CodeAgent(tools=[guest_info_tool], model=model)
-
-# Example query Alfred might receive during the gala
-response = alfred.run("Tell me about our guest named 'Lady Ada Lovelace'.")
-
-print("🎩 Alfred's Response:")
-print(response)
-
 
 # Initialize the tool
 search_tool = DuckDuckGoSearchTool()
@@ -51,9 +41,33 @@ hub_stats_tool = HubStatsTool()
 
 # Create Alfred with all the tools
 alfred = CodeAgent(
-    tools=[search_tool, weather_info_tool, hub_stats_tool], 
-    model=model
+    tools=[guest_info_tool, weather_info_tool, hub_stats_tool, search_tool],
+    model=model,
+    add_base_tools=True,  # Add any additional base tools
+    planning_interval=3,  # Enable planning every 3 steps
 )
 
-# Example query Alfred might receive during the gala
-response = alfred.run("What is Facebook and what's their most popular model?")
+query = "Tell me about 'Lady Ada Lovelace'"
+response = alfred.run(query)
+
+print("🎩 Alfred's Response:")
+print(response)
+
+query = "What's the weather like in Paris tonight? Will it be suitable for our fireworks display?"
+response = alfred.run(query)
+
+print("🎩 Alfred's Response:")
+print(response)
+
+query = "One of our guests is from Qwen. What can you tell me about their most popular model?"
+response = alfred.run(query)
+
+print("🎩 Alfred's Response:")
+print(response)
+
+
+query = "I need to speak with Dr. Nikola Tesla about recent advancements in wireless energy. Can you help me prepare for this conversation?"
+response = alfred.run(query)
+
+print("🎩 Alfred's Response:")
+print(response)
